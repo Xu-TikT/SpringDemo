b56f936fbcb3852f28bce2186981a5b0299908a7
xwt <710323287@qq.com>
2019-02-16 14:12:40 +0800

    增加了注销功能

--- src/main/java/com/example/demo/CustomPasswordEncoder.java
+++ src/main/java/com/example/demo/CustomPasswordEncoder.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+/**
+ * 自定义的Spring Security加密方式，这里使用的是明文
+ */
 public class CustomPasswordEncoder implements PasswordEncoder {
     @Override
     public String encode(CharSequence charSequence) {
--- src/main/java/com/example/demo/web/HelloController.java
+++ src/main/java/com/example/demo/web/HelloController.java
@@ -1,15 +1,16 @@
 package com.example.demo.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping("/hello")
     public String index() throws Exception{
-        return "Hello World";
+        return "hello";
 //        throw new Exception("发生错误");
     }
 
